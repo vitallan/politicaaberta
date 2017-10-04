5fda4f81f7e7d0a2278e7ffee1edd6d15304acc9
Allan Vital <cin_avital@uolinc.com>
2017-10-04 20:56:23 -0300

    adicionando novos endpoints

--- routes/deputy.js
+++ routes/deputy.js
@@ -6,4 +6,9 @@ router.get('/', function(req, res) {
   res.send('respond with a resource');
 });
 
+router.post('/', function(req, res) {
+  console.log(req.body.asdad);
+  res.send(req.body);
+});
+
 module.exports = router;
